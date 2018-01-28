61335bd6c0e9b3f514942e677c992cc087806130
alvindie <alvindiedevelop@gmail.com>
2018-01-28 14:56:51 -0500

    se termino el nucleo

---
+++ GlobalGameJam2018/Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public float maxTime = 120;
+
+	public int peoplesLeft;
+	public List<GameObject> zombies;
+	public List<GameObject> peobles;
+	private GameObject[] _peobles;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		_peobles = GameObject.FindGameObjectsWithTag("Persons");
+		
+		for (int i = 0; i < _peobles.Length; i++)
+		{
+			peobles.Add(_peobles[i]);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		peoplesLeft = peobles.Count;
+		
+		for (int i = 0; i < peobles.Count; i++)
+		{
+			if (peobles[i].GetComponent<PlayerController>().isZombie)
+			{
+				peobles.Remove(peobles[i]);
+			}
+		}
+
+		if (peoplesLeft <= 0)
+		{
+			//Win
+		}
+	}
+}
--- GlobalGameJam2018/Assets/Scripts/Player/PlayerController.cs
+++ GlobalGameJam2018/Assets/Scripts/Player/PlayerController.cs
@@ -11,7 +11,8 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
 	//datos
-	public float speed = 10f;
+	public float speed = 5f;
+	public float runSpeed = 10f;
 	public float rotationSpeed = 450f;
 	
 	//stats
@@ -20,12 +21,33 @@ public class PlayerController : MonoBehaviour
 	public bool isZombie = false;
 	
 	//AI
+	public bool isCop = false;
 	public GameObject[] waypoints;
+	public GameObject[] runWaypoints;
+	public enum AISTATE
+	{
+		Patrol,
+		AtackPlayer,
+		Run
+	}
+
+	
+	
+	//GUI
+	public Image panelHit; //panel para hacer efecto de hit
+	private Color curColorPanelHit; //el color neutral de panel
+	public Color colorHit; //el nuevo color del panel de hit
+
+	public AISTATE aiState = AISTATE.Patrol;
+	public float attackRage = 2.5f;
+	public float rateOfFire = 0.15f; //velocidad de disparo
+	public float myTimerToShoot = 0f; //el tiempo guardado para poder disparar.
 
 	private Rigidbody _rigidbody;
 	private CapsuleCollider _capsuleCollider;
 	private Quaternion _targetRotation;
 	private NavMeshAgent _navMeshAgent;
+	private GameManager _gameManager;
 	
 	
 
@@ -36,6 +58,7 @@ public class PlayerController : MonoBehaviour
 		_capsuleCollider = GetComponent<CapsuleCollider>();
 		_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
 		_navMeshAgent = GetComponent<NavMeshAgent>();
+		_gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
 		
 		//AI
 		if (!isZombie)
@@ -49,6 +72,7 @@ public class PlayerController : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
+		myTimerToShoot += Time.deltaTime; //acumula el timer
 
 		if (isZombie)
 		{
@@ -83,18 +107,26 @@ public class PlayerController : MonoBehaviour
 	//detectar collisiones
 	private void OnCollisionEnter(Collision other)
 	{
-		//Nuestro jugador
-		if (other.collider.tag == "Persons" || other.collider.tag == "Cop" && this.tag == "Player")
+
+		if (this.isZombie)
 		{
-			other.collider.GetComponent<PlayerController>().ApplyDamage(damage);
+			//Nuestro jugador
+			if (other.collider.tag == "Persons" || other.collider.tag == "Cop" && this.tag == "Player")
+			{
+				if (!other.collider.GetComponent<PlayerController>().isZombie)
+				{
+					other.collider.GetComponent<PlayerController>().ApplyDamage(damage);
+				}
+			}
 		}
-		
-		//los polis
-		if (other.collider.tag == "Player" && this.tag == "Cop" && !this.isZombie)
+		else
 		{
-			other.collider.GetComponent<PlayerController>().ApplyDamage(damage);
+			//los polis
+			if (other.collider.tag == "Player" && this.tag == "Cop" && !this.isZombie)
+			{
+				other.collider.GetComponent<PlayerController>().ApplyDamage(damage);
+			}
 		}
-		
 	}
 	
 	//Aplica el daño a nuestro personaje
@@ -103,7 +135,7 @@ public class PlayerController : MonoBehaviour
 		if (curHealth - damage > 0)
 		{
 			curHealth -= damage;
-			//StartCoroutine(GetHit());
+			StartCoroutine(GetHit());
 		}
 		else
 		{
@@ -111,7 +143,30 @@ public class PlayerController : MonoBehaviour
 			Death();
 		}
 	}
-
+	
+	//get hit
+	IEnumerator GetHit()
+	{
+		if (this.isZombie)
+		{
+			panelHit.color = colorHit;
+			yield return new WaitForSeconds(0.2f); //esperamos 0.2 segundos para poner el efecto de hit
+			panelHit.color = curColorPanelHit;
+		}
+		else
+		{
+			if (!isCop)
+			{
+				aiState = AISTATE.Run;
+				_navMeshAgent.speed = runSpeed;
+				_navMeshAgent.angularSpeed = 800;
+				_navMeshAgent.SetDestination(runWaypoints[Random.Range(0, runWaypoints.Length)].transform.position);
+			}
+		}
+		
+	}
+	
+	//is death
 	public void Death()
 	{
 		if (this.tag == "Persons" || this.tag == "Cop" && !this.isZombie)
@@ -119,10 +174,14 @@ public class PlayerController : MonoBehaviour
 			_navMeshAgent.Stop();
 			TransformZombie(this.transform, Color.green);
 			this.isZombie = true;
+			_gameManager.zombies.Add(this.gameObject);
 		}
 		else
 		{
-			Debug.Log("Player is DEATH!!");
+			if (this.tag == "Player")
+			{
+				//LOOSE
+			}
 		}
 	}
 	
@@ -138,10 +197,41 @@ public class PlayerController : MonoBehaviour
 
 	void AIcontroller()
 	{
-		Debug.Log(_navMeshAgent.remainingDistance);
-		if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= 0.5f)
+
+		switch (aiState)
 		{
-			_navMeshAgent.destination = waypoints[Random.Range(0, waypoints.Length)].transform.position;
+			//patrol
+			case AISTATE.Patrol:
+				
+				if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= 0.5f)
+				{
+					_navMeshAgent.destination = waypoints[Random.Range(0, waypoints.Length)].transform.position;
+				}
+				break;
+				//run
+			case AISTATE.Run:
+					
+					if (!_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= 0.5f)
+					{
+						_navMeshAgent.destination = runWaypoints[Random.Range(0, runWaypoints.Length)].transform.position;
+					}
+					break;
+					
+			case AISTATE.AtackPlayer:
+					_navMeshAgent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+					if ( !_navMeshAgent.pathPending && _navMeshAgent.remainingDistance <= attackRage)
+					{
+						if (myTimerToShoot >= rateOfFire && Time.deltaTime != 0)
+						{
+							//reiniciamos nuestro timer para volver a disparar
+							myTimerToShoot = 0;
+							GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().ApplyDamage(damage);
+						}
+					}
+					
+					break;
 		}
+		
 	}
 }
