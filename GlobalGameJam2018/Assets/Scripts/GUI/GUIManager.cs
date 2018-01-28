1f8969d4631f8f6c73c4c4b485a0087e41b986d0
niozmaster <jesus.fuentes@utp.ac.pa>
2018-01-28 12:32:57 -0500

    GUI Intro - Complete

---
+++ GlobalGameJam2018/Assets/Scripts/GUI/GUIManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+    //Public Variable
+    public GameObject main; //to get main panel visible
+    public GameObject credits; //to get credits panel visible
+    public GameObject howtoPlay; //to get how to play panel visible
+
+    //Private variables
+    private bool isVisible = false; //to manage the visualization of Game's Instrution
+
+    public void StartGame() {
+        //Application.LoadLevel("Mecanicas");
+        SceneManager.LoadScene("Mecanicas");
+    }
+
+    public void HowToPlay() {
+        main.SetActive(false);
+        howtoPlay.SetActive(true);
+    }
+
+    public void ReturninCredits()
+    {
+        main.SetActive(true);
+        credits.SetActive(false);
+    }
+
+    public void ReturninHowToPlay()
+    {
+        main.SetActive(true);
+        howtoPlay.SetActive(false);
+    }
+
+    public void Credits(){
+        main.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+    }
+	
+}
